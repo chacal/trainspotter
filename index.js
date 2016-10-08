877240814380e4f56088e29d8ace871528d97803
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-10-08 21:55:49 +0300

    Get train departure times dynamically from Digitraffic API

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict'
 var Alexa = require('alexa-sdk')
+var BPromise = require('bluebird')
+var request = BPromise.promisifyAll(require('request').defaults({ json: true }))
+var _ = require('lodash')
+var moment = require('moment')
+require('moment-round')
+require('moment-timezone')
 
 var APP_ID = "amzn1.ask.skill.fb8ceabb-21b7-4a1f-a554-51f3b6cc5657"
 var SKILL_NAME = 'Train Spotter'
@@ -19,8 +25,9 @@ var handlers = {
     this.emit('GetTrains')
   },
   "GetTrains": function () {
-    var speechOutput = "The next train leaves at 22:18 - that is 15 minutes from now. The one after that goes at 22:45."
-    this.emit(':tellWithCard', speechOutput, SKILL_NAME, speechOutput)
+    getTrainDepartureTimes()
+      .then(getSpeechOutput)
+      .then(output => this.emit(':tellWithCard', output, SKILL_NAME, output))
   },
   'AMAZON.HelpIntent': function () {
     var speechOutput = "You can say tell me a space fact, or, you can say exit... What can I help you with?"
@@ -32,5 +39,34 @@ var handlers = {
   },
   'AMAZON.StopIntent': function () {
     this.emit(':tell', 'Goodbye!')
+  },
+  'AMAZON.RepeatIntent': function () {
+    this.emit('GetTrains')
   }
 }
+
+function getTrainDepartureTimes() {
+  return request.getAsync('http://rata.digitraffic.fi/api/v1/live-trains?station=KEA&arrived_trains=0&arriving_trains=0&departed_trains=0&departing_trains=10')
+    .then(res => res.body.filter(notCancelledTowardsHki))
+    .then(trains => _.sortBy(trains.map(train => getDepartureTime(timeTableForKera(train))).filter(notAlreadyGone)))
+
+  function notCancelledTowardsHki(train) { return !train.cancelled && train.timeTableRows[0].stationShortCode !== 'HKI' }
+  function timeTableForKera(train) { return _.find(train.timeTableRows, row => row.stationShortCode === 'KEA') }
+  function getDepartureTime(train) { return train.liveEstimateTime || train.scheduledTime }
+  function notAlreadyGone(departureTime) { return moment().isBefore(departureTime) }
+}
+
+function getSpeechOutput(departureTimes) {
+  var departureTime1 = departureTimeText(departureTimes[0])
+  var departureDuration1 = moment(departureTimes[0]).fromNow()
+
+  return `<p>The next train leaves at ${departureTime1} - that is ${departureDuration1}.</p><p>The one after that goes at ${departureTimeText(departureTimes[1])}.</p>`
+
+  function departureTimeText(timestamp) {
+    return moment(timestamp).round(1, 'minutes').tz("Europe/Helsinki").format('kk:mm');
+  }
+}
+//
+//getTrainDepartureTimes()
+//  .then(getSpeechOutput)
+//  .then(output => console.log(output))
